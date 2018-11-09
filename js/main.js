aafe278593612d156e0768ef722e90e605474968
Jesus Munoz Ramirez <jmunoz@rosen-group.com>
2018-11-09 12:41:37 -0500

    Maps enabled

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoiam11bm96ciIsImEiOiJjam9hN3FzaTkwY2oxM3VtdnNncGs0c2xyIn0.v5hgcvRjlqAe6CGles1dTg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
