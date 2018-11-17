d7a9e52f0f692be6481d2e8c452e503dba68f296
Jesus Munoz <jesus.munozr@gmail.com>
2018-11-17 09:08:24 -0500

    Aria roles added for maps and semantic are fixed

--- js/main.js
+++ js/main.js
@@ -187,7 +187,7 @@ createRestaurantHTML = (restaurant, tabIndex) => {
   image.alt = `Restaurant: ${restaurant.name}.`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
